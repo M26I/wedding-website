16a0b82b218ed9c61ece39fcf67c3aafbc4bee0b
M26I <marija.ivanisevic267@gmail.com>
2023-10-12 10:39:29 +0100

    firs commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    
+    
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "maingreen": "#1D3539",
+        "textb": "#C7A385",
+        "offw" : "#EEE9E4"
+      }
+    },
+  },
+  plugins: [],
+}
