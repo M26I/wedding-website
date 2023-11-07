7d906f61180307bfc1b3bf706d23bf9e880ffb5a
M26I <marija.ivanisevic267@gmail.com>
2023-11-07 14:10:31 +0000

    next/script

--- src/components/map.js
+++ src/components/map.js
@@ -4,9 +4,9 @@ import Script from "next/script";
 function GoogleMap() {
   return (
     <>
-      <Head>
+     
         <Script async defer src="https://maps.googleapis.com/maps/api/js"></Script>
-      </Head>
+      
       <div className="w-full max-w-screen-sm mx-auto md:max-w-screen-md lg:max-w-screen-lg">
         <div
           className="w-full md:aspect-w-16 md:aspect-h-12 lg:aspect-w-16 lg:aspect-h-10"
