76ab15d40189333ac6b869913bd5b7442ac52c7b
M26I <marija.ivanisevic267@gmail.com>
2023-11-09 11:10:18 +0000

    mongodbtrigger

--- src/pages/api/submit-rsvp.js
+++ src/pages/api/submit-rsvp.js
@@ -7,13 +7,7 @@ export default async function handler(req, res) {
   
       try {
         await sendToConfirmedCollection(formData);
-        await fetch('https://peaceful-mochi-06dba0.netlify.app/api/mongodb-webhook', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(formData),
-        });
+       
         res.status(201).end();
       } catch (error) {
         console.error("Error submitting RSVP:", error);
