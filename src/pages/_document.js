265f304b0ec6b3a5f5b20c8c355bef6d57eb9b0d
M26I <marija.ivanisevic267@gmail.com>
2024-04-01 13:17:11 +0100

    og properties

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+      <meta property="og:title" content="Our wedding website - Marija & Liam" />
+        <meta property="og:description" content="Please join us as we celebrate beginning our new adventure" />
+      <meta property="og:image" content="https://ivanisevic-wallace-wedding.netlify.app/_next/image?url=%2Fcollage4.webp&w=1920&q=75" />
+      <meta property="og:image:width" content="600" />
+      <meta property="og:image:height" content="315" />
+      <meta property="og:type" content="website" />
+      </Head>
       <body>
         <Main />
         <NextScript />
